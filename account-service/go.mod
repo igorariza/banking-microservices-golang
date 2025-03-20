7133713749cbff269fb17ad4d608068dcf7528d4
igorariza <igorariza@gmail.com>
2025-03-20 18:41:10 -0500

    Add circuit breaker to account service for improved error handling

--- account-service/go.mod
+++ account-service/go.mod
@@ -35,6 +35,7 @@ require (
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/sony/gobreaker v1.0.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
--- account-service/services/account_service.go
+++ account-service/services/account_service.go
@@ -6,51 +6,87 @@ import (
 	"banking-system/account-service/utils"
 	"context"
 	"errors"
+	"log"
+	"time"
 
+	"github.com/sony/gobreaker"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type AccountService struct {
 	Client   *mongo.Client
 	Database *mongo.Database
+	Cb       *gobreaker.CircuitBreaker
 }
 
 func NewAccountService(db *mongo.Database) *AccountService {
+
+	cbSettings := gobreaker.Settings{
+		Name:        "AccountServiceCircuitBreaker",
+		MaxRequests: 5,
+		Interval:    time.Second * 60,
+		Timeout:     time.Second * 10,
+		ReadyToTrip: func(counts gobreaker.Counts) bool {
+			failureRatio := float64(counts.TotalFailures) / float64(counts.Requests)
+			return counts.Requests >= 5 && failureRatio >= 0.6
+		},
+		OnStateChange: func(name string, from gobreaker.State, to gobreaker.State) {
+			log.Printf("State Change: %s: %s -> %s", name, from, to)
+		},
+	}
+	cb := gobreaker.NewCircuitBreaker(cbSettings)
+
 	return &AccountService{
 		Client:   db.Client(),
 		Database: db,
+		Cb:       cb,
 	}
 }
 
 func (s *AccountService) CreateAccount(account models.Account) (*models.Account, error) {
-	create, err := data.CreateAccount(s.Client, &account)
-	if err != nil {
-		return nil, err
-	}
-	utils.PublishAccountEvent(context.Background(), account.ID)
+	acc, err := s.Cb.Execute(func() (interface{}, error) {
+		create, err := data.CreateAccount(s.Client, &account)
+		if err != nil {
+			return nil, err
+		}
+		utils.PublishAccountEvent(context.Background(), account.ID)
 
-	return &models.Account{
-		ID:       create.ID,
-		Name:     create.Name,
-		Balance:  create.Balance,
-		CreateAt: create.CreateAt,
-		UpdateAt: create.UpdateAt,
-	}, nil
+		return create, nil
+	})
+
+	if err != nil {
+		log.Printf("Error creating account: %v", err)
+		return nil, errors.New("service unavailable")
+	}
+
+	return acc.(*models.Account), nil
 }
 
 func (s *AccountService) GetAccountBalance(id string) (float64, error) {
 	var account models.Account
 	account.ID = id
-	bl, err := data.GetAccountBalance(s.Client, account)
-	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			return 0, errors.New("account not found")
+	result, err := s.Cb.Execute(func() (interface{}, error) {
+		bl, err := data.GetAccountBalance(s.Client, account)
+		if err != nil {
+			if err == mongo.ErrNoDocuments {
+				return 0, errors.New("account not found")
+			}
+			return 0, err
 		}
-		return 0, err
-	}
-	account.Balance = bl	
+		account.Balance = bl
 
-	utils.PublishAccountEvent(context.Background(), account.ID)
+		utils.PublishAccountEvent(context.Background(), account.ID)
+		return account, nil
+	})
+
+	if err == nil {
+		account = result.(models.Account)
+	}
+
+	if err != nil {
+		log.Printf("Error getting account balance: %v", err)
+		return 0, errors.New("service unavailable")
+	}
 
 	return account.Balance, nil
 }
